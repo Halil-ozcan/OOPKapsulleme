46cccdde2aefa52e78c158516ca9bd4402bb6201
Halil Özcan <halil_ozcann@outlook.com>
2024-12-24 22:39:20 +0300

    C# OOP Kapsülleme Proje 1 Yapıldı

---
+++ KapsullemeProje1/Musteri.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeProje1
+{
+    internal class Musteri
+    {
+        private string Ad;
+        private string Soyad;
+        private int yas;
+
+        public Musteri(string ad, string soyad, int yas)
+        {
+           this.Ad = ad;
+           this.Soyad = soyad;
+            SetYas(yas);
+        }
+
+        public string GetAdSoyad()
+        {
+            return $"{Ad} {Soyad}";
+        }
+
+        public void SetYas(int yeniYas)
+        {
+            if(yeniYas < 0)
+            {
+                Console.WriteLine("Yaş Negatif Olamaz! Yaş Sıfır Olarak Ayarlandı");
+                yas = 0;
+            }
+            else
+            {
+                yas = yeniYas;
+            }
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+
+    }
+}
--- KapsullemeProje1/Program.cs
+++ KapsullemeProje1/Program.cs
@@ -10,6 +10,21 @@ namespace KapsullemeProje1
     {
         static void Main(string[] args)
         {
+            Musteri kisi1 = new Musteri("Ahmet", "Yılmaz", 25);
+            Musteri kisi2 = new Musteri("Ayşe", "Kara", -5); 
+            Musteri kisi3 = new Musteri("Mehmet", "Demir", 45);
+
+            Console.WriteLine($"Ad-Soyad: {kisi1.GetAdSoyad()}, Yaş: {kisi1.GetYas()}");
+            Console.WriteLine($"Ad-Soyad: {kisi2.GetAdSoyad()}, Yaş: {kisi2.GetYas()}");
+            Console.WriteLine($"Ad-Soyad: {kisi3.GetAdSoyad()}, Yaş: {kisi3.GetYas()}");
+
+
+            Console.WriteLine("\nAyşe'nin yaşını güncellemek istiyoruz...");
+            kisi2.SetYas(30);
+            Console.WriteLine($"Ad-Soyad: {kisi2.GetAdSoyad()}, Güncel Yaş: {kisi2.GetYas()}");
+
+
+
         }
     }
 }
