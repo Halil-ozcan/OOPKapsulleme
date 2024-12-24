03f00aae8500412ad4a54839b0e8894f91d40bec
Halil Özcan <halil_ozcann@outlook.com>
2024-12-24 20:38:41 +0300

    C# OOP Sinif Kapsulleme Yapıldı

---
+++ OOPKapsulleme/Musteri.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPKapsulleme
+{
+    internal class Musteri
+    {
+
+        public Musteri() 
+        {
+            this.id = IDUret();
+        }
+
+
+        // Bunlar Class => Field olarak geçer.
+        int id;
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+
+            private set // sadece değer okunabilsin değer ataması yapılmasın diye private yapıyoruz.
+            {
+                this.id = value;
+            }
+        }
+        public string isim;
+        public string soyisim;
+
+
+        public string emailAddress;
+
+        public string EmailAddress
+        {
+            //get; // database veya farklı bir veri kaynagı Classı  çağıran programcıya datayı gösterdiğimiz kısım.
+            //set; // dış dünyadan alinan datanın içeride private olarak saklanan field içerisine deger atandığı kısım.
+
+            set
+            {
+                this.emailAddress = value;
+            }
+
+            get
+            { 
+                return this.emailAddress;
+            }
+        }
+
+        private int IDUret()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10000,90000);
+        }
+    }
+}
--- OOPKapsulleme/Program.cs
+++ OOPKapsulleme/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,26 @@ namespace OOPKapsulleme
     {
         static void Main(string[] args)
         {
+            
+
+            Musteri musteri = new Musteri();
+            //musteri.id = 1;
+            //musteri.isim = "Halil";
+            //musteri.soyisim = "Özcan";
+
+
+            // field değer ataması yapılmasın ama değer okunabilsin.
+            // field değer ataması yapsın ama değer okuyamasın.
+            // field değer ataması yapılsın ama ilk 4hanesi okunsun.
+
+            musteri.emailAddress = "Halil@gmail.com";
+
+            Console.WriteLine(musteri.EmailAddress);
+
+
+            // 1. field deger ataması yapılamsın ama deger okunabilsin.
+            Console.WriteLine("Musteri ID Degeri : " + musteri.ID.ToString());
+            
         }
     }
 }
