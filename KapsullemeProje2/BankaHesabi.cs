920f7a5d6e8051c114ed5eec189e52ae93147dcd
Halil Özcan <halil_ozcann@outlook.com>
2024-12-25 20:29:16 +0300

    C# OOP Sinif Kapsulleme Proje 2 Yapıldı

---
+++ KapsullemeProje2/BankaHesabi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeProje2
+{
+    internal class BankaHesabi
+    {
+        string HesapNo;
+        double Bakiye;
+
+
+        public BankaHesabi(string hesapNo, double baslangicBakiyesi)
+        {
+            HesapNo = hesapNo;
+            if(baslangicBakiyesi<0)
+            {
+                Console.WriteLine("Başlangıc Bakiyesi negatif Olamaz. Bakiye Sifir Olarak Ayarlandı.");
+                this.Bakiye = 0;
+            }
+            else
+            {
+                this.Bakiye=baslangicBakiyesi;
+            }
+        }
+
+        public void ParaYatir(double miktar)
+        {
+            if(miktar<0)
+            {
+                Console.WriteLine("Hata: Yatiralacak Miktar Negatif veya Sıfır Olamaz");
+
+            }
+            else
+            {
+                Bakiye += miktar;
+                Console.WriteLine($"{miktar} TL hesabınıza yatırıldı. Güncel Bakiye : {Bakiye} TL.");
+            } 
+        }
+
+        public void ParaCek(double miktar)
+        {
+            if (miktar < 0)
+            {
+                Console.WriteLine("Hata: Yatiralacak Miktar Negatif veya Sıfır Olamaz");
+
+            }
+            else if(miktar > Bakiye)
+            {
+                Console.WriteLine("Hata: Hesap bakiyesi yetersiz. İşlem yapılamadı.");
+            }
+            else
+            {
+                Bakiye -= miktar;
+                Console.WriteLine($"{miktar} TL hesabınızdan Çekildi. Güncel Bakiye : {Bakiye} TL.");
+            }
+        }
+
+        public double GetBakiye()
+        {
+            return Bakiye; 
+        }
+
+       
+    }
+}
--- KapsullemeProje2/Program.cs
+++ KapsullemeProje2/Program.cs
@@ -10,6 +10,21 @@ namespace KapsullemeProje2
     {
         static void Main(string[] args)
         {
+            BankaHesabi bankaHesabi1 = new BankaHesabi("123456", 500);
+            BankaHesabi bankaHesabi2 = new BankaHesabi("789101", 1000);
+
+            Console.WriteLine("Banka Hesabi 1 ");
+            bankaHesabi1.ParaYatir(200);
+            bankaHesabi1.ParaCek(100);
+
+            Console.WriteLine("Banka Hesabi 2");
+            bankaHesabi2.ParaCek(1200);
+            bankaHesabi2.ParaCek(500);
+
+
+            Console.WriteLine("\nSon Bakiyeler:");
+            Console.WriteLine($"Hesap 1 Bakiye: {bankaHesabi1.GetBakiye()} TL");
+            Console.WriteLine($"Hesap 2 Bakiye: {bankaHesabi2.GetBakiye()} TL");
 
         }
     }
